f751e727562c1010b09a098777b80eca30e477ea
Damilare Aleji <alejigideonabidemi@gmail.com>
2023-11-19 23:45:26 +0100

    final commit

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const notesContainer = document.querySelector(".notesContainer");
 
 const createBtn = document.querySelector(".btn");
 
-let notes = document.querySelectorAll(".inputBox")
+let notes = document.querySelectorAll(".inputBox");
+
+function showNotes () {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+function updateStorage () {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
 
 createBtn.addEventListener("click", ()=> {
     let inputBox = document.createElement("p");
@@ -11,4 +21,26 @@ createBtn.addEventListener("click", ()=> {
     inputBox.setAttribute("contenteditable", "true");
     img.src = "images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage ();
+    }
+    else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".inputBox");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
 })
